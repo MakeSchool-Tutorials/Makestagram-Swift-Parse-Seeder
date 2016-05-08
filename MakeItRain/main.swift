dc0b37354650aff6a50c8ca2e9f93534aea8ac29
jason <json@katzr.com>
2016-05-07 20:16:36 -0400

    Initial Commit

---
+++ MakeItRain/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MakeItRain
+//
+//  Created by Jason Katzer on 5/7/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
