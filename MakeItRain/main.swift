96b4c86cf8657b738f72f96f81175a688aee1cd6
jason <json@katzr.com>
2016-05-07 21:26:11 -0400

    initial commit

--- MakeItRain/main.swift
+++ MakeItRain/main.swift
@@ -8,5 +8,57 @@
 
 import Foundation
 
-print("Hello, World!")
+import Parse
 
+func setupParse() {
+  let configuration = ParseClientConfiguration {
+    $0.applicationId = "{{your-parse-app-name}}"
+    $0.server = "https://{{your-heroku-app-name}}.herokuapp.com/parse"
+  }
+  Parse.initializeWithConfiguration(configuration)
+}
+
+func randomUsername() -> String {
+  let names = ["bob", "cindy", "may", "charles", "javier"]
+  // get a name at random
+  let name = names[Int(arc4random_uniform(UInt32(names.count)))]
+  // get a 4 digit number at random
+  let number = arc4random_uniform(8999) + 1000
+
+  return "\(name)\(number)"
+}
+
+
+func makeFakeUser(username: String) {
+  let fakeUser = PFUser()
+  fakeUser.username = username
+  fakeUser.password = "\(username)doesnothaveapassword"
+  do{
+    try fakeUser.signUp()
+  } catch {
+    print("could not create user")
+    return
+  }
+
+
+  let acl = PFACL()
+  acl.publicReadAccess = true
+  acl.setWriteAccess(true, forUser: fakeUser)
+
+  let fakePost = PFObject(className: "Post")
+  fakePost.setObject(fakeUser, forKey: "user")
+  fakePost.ACL = acl
+
+  do {
+    try fakePost.save()
+  } catch {
+    print("could not create user")
+  }
+}
+
+print ("this app is single threaded, and does not have a UI, so we are going to block the main thread")
+print ("setting up parse")
+setupParse()
+print ("making a fake user")
+makeFakeUser(randomUsername())
+sleep(10)
---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'MakeItRain' do
+  pod 'Parse'
+end
+
